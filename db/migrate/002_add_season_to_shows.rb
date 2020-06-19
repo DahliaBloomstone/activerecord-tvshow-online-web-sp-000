637db1e96067674268bfee45000a1838e5ffd036
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 02:27:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,7 @@
 #write a migration to add a column, season, to the shows table
 #datatype of this column is a string
 #ActiveRecord::Migration[5.2]
-class AddSeasonToShows < ActiveRecord::Migration
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :season, :string
   end
