1aed76d7caa9b42987e5f19cbf61a097489fb863
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 02:27:05 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,14 @@
 #migration code to create a shows table
 #name, network, day, rating columns
-#datatype first 3, rating integer 
+#datatype first 3, rating integer
+
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
